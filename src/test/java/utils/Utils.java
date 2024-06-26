2668202c53e021172405e43de4dfbef9e4b2ccfc
al.nikitin <al.nikitin@astondevs.ru>
2024-06-26 11:14:41 +0300

    поменял на константы

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -12,27 +12,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
+import static constants.Constants.PATH_TO_CSV;
+import static constants.Constants.RU_LOCAL;
+import static utils.TransactionDetails.DATE;
+import static utils.TransactionDetails.AMOUNT;
+import static utils.TransactionDetails.TYPE;
+
 public class Utils {
 
     public static String formatterTransactionDate(String transactionDate) throws ParseException {
         DateFormat df = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss", Locale.ENGLISH);
         Date date = df.parse(transactionDate);
-        SimpleDateFormat newFormatter = new SimpleDateFormat("dd MMMM yyyy HH:mm:ss", Locale.forLanguageTag("ru"));
+        SimpleDateFormat newFormatter = new SimpleDateFormat("dd MMMM yyyy HH:mm:ss", Locale.forLanguageTag(RU_LOCAL));
         return newFormatter.format(date);
     }
 
-    public static HashMap<String ,String> parseTable(String transactionDate, String transactionAmount, String transactionType) throws ParseException {
+    public static HashMap<String ,String> parseTable(String transactionDate, String transactionAmount, String transactionType) {
         HashMap<String, String> transaction = new HashMap<>();
-        transaction.put("date", transactionDate);
-        transaction.put("amount", transactionAmount);
-        transaction.put("type", transactionType);
+        transaction.put(DATE.getDescription(), transactionDate);
+        transaction.put(AMOUNT.getDescription(), transactionAmount);
+        transaction.put(TYPE.getDescription(), transactionType);
         return transaction;
     }
 
     public static void writeFileCsv(List<HashMap<String, String>> allTransaction) throws IOException {
-        try(FileWriter csvWriter = new FileWriter("src/test/resources/new.csv")) {
+        try(FileWriter csvWriter = new FileWriter(PATH_TO_CSV)) {
             for (HashMap<String, String> rowData : allTransaction) {
-                csvWriter.append(String.format("%s %s %s", rowData.get("date"), rowData.get("amount"), rowData.get("type")));
+                csvWriter.append(String.format("%s %s %s", rowData.get(DATE.getDescription()),
+                        rowData.get(AMOUNT.getDescription()), rowData.get(TYPE.getDescription())));
                 csvWriter.append("\n");
             }
         }
