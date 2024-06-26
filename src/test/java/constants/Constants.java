bc49d4a5fd77f5a5a25e03948568a8707fa9c401
al.nikitin <al.nikitin@astondevs.ru>
2024-06-26 11:11:28 +0300

    создал класс для констант

---
+++ src/test/java/constants/Constants.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class Constants {
+
+    public static final String GLOBALQA_URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login";
+    public static final String DATE_FORMAT_WITHOUT_TIME = "dd MMMM yyyy";
+    public static final String RU_LOCAL = "ru";
+    public static final String TRANSACTION_TYPE_CREDIT = "Credit";
+    public static final String TRANSACTION_TYPE_DEBIT = "Debit";
+    public static final String PATH_TO_CSV = "src/test/resources/new.csv";
+    public static final int TWO_TRANSACTIONS = 2;
+}
